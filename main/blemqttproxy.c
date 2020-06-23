c5834545f33a502341a3a230662ae503a9643216
an-erd <anerd@gmx.de>
2020-06-23 12:03:03 +0200

    Update toESP-IDF Pre-release v4.1-beta2

--- main/blemqttproxy.c
+++ main/blemqttproxy.c
@@ -8,7 +8,7 @@
 
 #include "esp_wifi.h"
 #include "esp_system.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "esp_assert.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -30,7 +30,7 @@
 #include "errno.h"
 #include "esp_timer.h"
 #include "lwip/sockets.h"
-#include <tcpip_adapter.h>
+#include "esp_netif.h"
 #include "lwip/dns.h"
 #include "lwip/netdb.h"
 #include <inttypes.h>
@@ -142,7 +142,6 @@ void clear_stats_values()
 void button_push_cb(void* arg)
 {
     uint8_t btn = *((uint8_t*) arg);
-    UNUSED(btn);
 
     if(!display_status.button_enabled){
         ESP_LOGD(TAG, "button_push_cb: button not enabled");
@@ -434,36 +433,55 @@ void periodic_wdt_timer_callback(void* arg)
     }
 }
 
-static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
+static void event_handler(void* ctx, esp_event_base_t event_base,  int32_t event_id, void* event_data)
 {
     httpd_handle_t *server = (httpd_handle_t *) ctx;
     EventBits_t uxReturn;
 
-    switch (event->event_id) {
-        case SYSTEM_EVENT_STA_START:
-            esp_wifi_connect();
-            break;
-        case SYSTEM_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "wifi_event_handler: SYSTEM_EVENT_STA_GOT_IP, IP: '%s'", ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-            wifi_connections_count_connect++;
-            xEventGroupSetBits(wifi_evg, WIFI_CONNECTED_BIT);
-            break;
-        case SYSTEM_EVENT_STA_DISCONNECTED:
-            ESP_LOGI(TAG, "wifi_event_handler: SYSTEM_EVENT_STA_DISCONNECTED");
-            wifi_connections_count_disconnect++;
-            esp_wifi_connect();
-            xEventGroupClearBits(wifi_evg, WIFI_CONNECTED_BIT);
-            break;
-        case SYSTEM_EVENT_AP_STACONNECTED:
-            ESP_LOGI(TAG, "wifi_event_handler: SYSTEM_EVENT_AP_STACONNECTED, station:"MACSTR" join, AID=%d", MAC2STR(event->event_info.sta_connected.mac), event->event_info.sta_connected.aid);
-            wifi_ap_connections++;
-            break;
-        case SYSTEM_EVENT_AP_STADISCONNECTED:
-            ESP_LOGI(TAG, "wifi_event_handler: SYSTEM_EVENT_AP_STADISCONNECTED, station:"MACSTR"leave, AID=%d", MAC2STR(event->event_info.sta_disconnected.mac), event->event_info.sta_disconnected.aid);
-            wifi_ap_connections--;
-            break;
-        default:
-            break;
+    if (event_base == WIFI_EVENT){
+        switch(event_id){
+            case WIFI_EVENT_STA_START:
+                ESP_LOGI(TAG, "event_handler: WIFI_EVENT_STA_START");
+                esp_wifi_connect();
+                break;
+            case WIFI_EVENT_STA_CONNECTED:
+                ESP_LOGI(TAG, "event_handler: WIFI_EVENT_STA_CONNECTED");
+                break;
+            case WIFI_EVENT_STA_DISCONNECTED:
+                ESP_LOGI(TAG, "event_handler: WIFI_EVENT_STA_DISCONNECTED");
+                wifi_connections_count_disconnect++;
+                esp_wifi_connect();
+                xEventGroupClearBits(wifi_evg, WIFI_CONNECTED_BIT);
+                break;
+            case WIFI_EVENT_AP_STACONNECTED: {
+                wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+                ESP_LOGI(TAG, "event_handler: WIFI_EVENT_AP_STACONNECTED, station "MACSTR" join, AID=%d", MAC2STR(event->mac), event->aid);
+                wifi_ap_connections++;
+                }
+                break;
+            case WIFI_EVENT_AP_STADISCONNECTED: {
+                wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+                ESP_LOGI(TAG, "event_handler: WIFI_EVENT_AP_STADISCONNECTED, station "MACSTR" leave, AID=%d", MAC2STR(event->mac), event->aid);
+                wifi_ap_connections--;
+                }
+                break;
+            default:
+                break;
+        }
+    } else if (event_base == IP_EVENT){
+        ESP_LOGI(TAG, "event_handler: IP_EVENT: %d", event_id);
+
+        switch(event_id){
+            case IP_EVENT_STA_GOT_IP: {
+                ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+                ESP_LOGI(TAG, "event_handler: IP_EVENT_STA_GOT_IP, IP: " IPSTR, IP2STR(&event->ip_info.ip));
+                wifi_connections_count_connect++;
+                xEventGroupSetBits(wifi_evg, WIFI_CONNECTED_BIT);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     uxReturn = xEventGroupWaitBits(wifi_evg, WIFI_CONNECTED_BIT, false, true, 0);
@@ -479,20 +497,23 @@ static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
             *server = NULL;
         }
     }
-
-    return ESP_OK;
 }
 
 static void wifi_init(void *arg)
 {
     uint8_t mac[6];
     char buffer[32];
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, arg));
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_ap();
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
 
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, arg));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, arg));
     wifi_config_t wifi_config_sta, wifi_config_ap;
     memset(&wifi_config_sta, 0, sizeof(wifi_config_sta));
     memcpy((char *)wifi_config_sta.ap.ssid, CONFIG_WIFI_SSID, strlen(CONFIG_WIFI_SSID));
@@ -553,7 +574,8 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
         case MQTT_EVENT_ERROR:
             ESP_LOGD(TAG, "MQTT_EVENT_ERROR");
             break;
-        case MQTT_EVENT_ANY:
+        // case MQTT_EVENT_ANY:
+        default:
             ESP_LOGI(TAG, "MQTT_EVENT_ANY");
             break;
 
@@ -970,6 +992,8 @@ static void gattc_profile_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_
                 ESP_LOGD(TAG, "ESP_GATTC_NOTIFY_EVT, first, idx %d, ble_beacons[idx] %d, ble_beacons[idx].p_buffer_download[count] %d",
                     idx, (&ble_beacons[idx] != NULL),  (ble_beacons[idx].p_buffer_download != NULL) );
                 time_measure = esp_timer_get_time();
+            } else {
+                ESP_LOGD(TAG, "ESP_GATTC_NOTIFY_EVT, subsequent, count %d", count);
             }
             ble_beacons[idx].p_buffer_download[count].sequence_number  = uint16_decode(&p_data->notify.value[len]); len += 2; // uint16_t
             ble_beacons[idx].p_buffer_download[count].time_stamp       = uint32_decode(&p_data->notify.value[len]); len += 4; // time_t
@@ -1520,15 +1544,15 @@ void adjust_log_level()
     esp_log_level_set("esp_ota_ops", ESP_LOG_WARN);
     esp_log_level_set("boot_comm", ESP_LOG_WARN);
     esp_log_level_set("wifi", ESP_LOG_INFO);
-    esp_log_level_set("BT_BTM", ESP_LOG_WARN);
+    esp_log_level_set("BT_BTM", ESP_LOG_INFO);
     esp_log_level_set("MQTT_CLIENT", ESP_LOG_INFO);
     esp_log_level_set("timer", ESP_LOG_INFO);
     esp_log_level_set("beacon", ESP_LOG_INFO);
     esp_log_level_set("display", ESP_LOG_INFO);
     esp_log_level_set("event", ESP_LOG_INFO);
     esp_log_level_set("ble_mqtt", ESP_LOG_INFO);
-    esp_log_level_set("web_file_server", ESP_LOG_INFO);
-    esp_log_level_set("BLEMQTTPROXY", ESP_LOG_INFO);
+    esp_log_level_set("web_file_server", ESP_LOG_DEBUG);
+    esp_log_level_set("BLEMQTTPROXY", ESP_LOG_DEBUG);
 }
 
 void initialize_ble()
